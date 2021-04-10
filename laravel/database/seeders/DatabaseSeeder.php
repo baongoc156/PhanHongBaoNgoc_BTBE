1612ab457a0993db8d039fd15287419f5832b28e
webcms <phbaongoc2001@gmail.com>
2021-04-10 21:53:20 +0700

    laravel

--- laravel/database/seeders/DatabaseSeeder.php
+++ laravel/database/seeders/DatabaseSeeder.php
@@ -17,8 +17,8 @@ class DatabaseSeeder extends Seeder
         
         $this->call([
             
-            CompaniesSeeder::class,
-            TrainersSeeder::class,
+            CompanySeeder::class,
+            TrainerSeeder::class,
           ]);
     }
 }
